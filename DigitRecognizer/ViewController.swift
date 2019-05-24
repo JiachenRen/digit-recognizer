0507bc6ca8e1c562141643a608ec17ad915a6122
jiachenren <arc4jiachenren@gmail.com>
2019-05-23 22:37:01 -0400

    Initial Commit

---
+++ DigitRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DigitRecognizer
+//
+//  Created by Jiachen Ren on 5/23/19.
+//  Copyright © 2019 Jiachen Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
